6b954bf945dc968128a9011dca7aa94c92840e43
Angel <43154124+angel-utn@users.noreply.github.com>
2025-04-02 20:10:23 -0300

    Create suma.cpp

---
+++ 20250326/suma.cpp
@@ -0,0 +1,21 @@
+///Ejercicio:
+///Autor:DEK
+///Fecha:
+///Comentario:
+
+# include<iostream>
+using namespace std;
+
+int main(){
+    int n1, n2, suma;
+    cout<<"INGRESAR NUMERO ";
+    cin>>n1;
+    cout<<"INGRESAR NUMERO ";
+    cin>>n2;
+    cout<<"EL RESULTADO DE LA SUMA ES ";
+    suma=n1+n2;
+    cout<<suma;
+	cout<<endl;
+	system("pause");
+	return 0;
+}
